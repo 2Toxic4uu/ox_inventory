e101335861d028f278cc1800419c27b749d4bb17
2Toxic4uu <115114280+2Toxic4uu@users.noreply.github.com>
2024-05-27 22:04:23 +0200

    containers

--- modules/items/containers.lua
+++ modules/items/containers.lua
@@ -63,4 +63,42 @@ setContainerProperties('pizzabox', {
 	whitelist = { 'pizza' }
 })
 
+setContainerProperties('cardholder', {
+	slots = 30,
+	maxWeight = 0,
+	whitelist = { 'keycardbellaapartment', "keycardbunker", "keycardbunker", "keycardtyhaus", "keycardcabi",
+		"keycard6068lamar", "keycardairport", "keycard9075", "keycardeinreise", "keycard7032", "keycardstudio",
+		"keycard8209", "keycardunicorn", "keycard7319", "keycard73304", "keycard73302", "keycard73301", 'keycard73305',
+		'keycard73307', "keycard6113", "keycard6157", "keycard7055", "keycard6070", "keycard6027", "keycard6190",
+		"keycard5014", "keycard5013b", "keycard5009", "keycard5006", "keycardtinyhome", "keycard5012", "keycardmaster",
+		"keycard4009", "keycard7037", "keycard5000", "keycard5019a", "keycard6150", "keycard6013", "keycardcayo",
+		"keycardlombank18", 'keycardmirrors', 'keycardhenhouse', 'keycard2056a', 'keycardvineyard', 'keycardtinyhome',
+		'keycardhole', 'keycard9055', 'keycard6067' }
+})
+
+setContainerProperties('burger_box_tate', {
+	slots = 10,
+	maxWeight = 10000,
+	whitelist = { 'tateburger_ribs', 'tateburger_wings', 'tateburger_nuggets', 'fries_regular', 'fries_waffle',
+		'fries_cheese_bacon', 'fries_cheese_chilli', 'mozzarella_sticks', 'tateburger_pancackes', 'tateburger_orginal',
+		'tateburger_double', 'tateburger_chilli_cheese', 'the_pop', 'tateburger_nitro', 'tateburger_wrap',
+		'tateburger_energy_nitro', 'tateburger_energy_hydro', 'milkshake_choc', 'milkshake_straw', 'milkshake_vanil' }
+})
+
+setContainerProperties('pingubox', {
+	slots = 10,
+	maxWeight = 10000,
+	whitelist = { 'donut_pingu', 'black_tea', 'apple_tea', 'fruity_tea', 'peppermint_tea', 'hot_chocolate', 'chai_tea',
+		'coffeetogo', 'cola', 'cola_cherry', 'cola_zero', 'sprite', 'fanta' }
+})
+
+setContainerProperties('bento_box', {
+	slots = 10,
+	maxWeight = 10000,
+	whitelist = { 'black_tea', 'apple_tea', 'fruity_tea', 'peppermint_tea', 'hot_chocolate', 'chai_tea', 'coffeetogo',
+		'cola', 'cola_cherry', 'cola_zero', 'sprite', 'fanta', 'matcha_coffee ', 'donut_cat', 'tacos', 'salmon_bagel',
+		'tuna_sandwich', 'warm_soup', 'curry_rice', 'omlette_egg', 'strawberry_shortcake', 'cat_macaroon_brown',
+		'cat_macaroon_green', 'cat_macaroon_turquoise', 'cat_macaroon_pink', 'pancakes' }
+})
+
 return containers
